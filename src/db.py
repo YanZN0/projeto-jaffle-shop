74ffa4417243eba9092060bdb7bfe49455da3d9c
Yan Robim <yanrobim@gmail.com>
2025-03-14 09:58:58 -0300

    conexão com dataset estabelecida!

--- src/db.py
+++ src/db.py
@@ -1,14 +1,15 @@
 ##  Código para conseguirmos conexão com o nosso Cloud de armazenamento com o SQLAlchemy-BigQuery.
 
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-project_id = "{{ env_var('PROJECT_ID') }}"
+project_id = os.getenv("PROJECT_ID")
+project_dataset = os.getenv("PROJECT_DATASET")
 
 
 engine = create_engine(
-    f"bigquery://{project_id}"
+    f"bigquery://{project_id}/{project_dataset}"
 )  ## Fazendo a conexão ao banco pelo ID do projeto.
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
